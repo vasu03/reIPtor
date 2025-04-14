6da21ddda1789520a56b479ce95fc44b31805007
vasu03 <vasurmakadia@gmail.com>
2025-04-14 18:12:38 +0530

    [update] : Optimized code for runtime efficiency

--- reiptor.sh
+++ reiptor.sh
@@ -20,10 +20,13 @@
 ##########################################
 # Color Definitions for Terminal Output  #
 ##########################################
+BOLD_RED='\033[1;31m'
 RED='\033[0;31m'
+BOLD_GREEN='\033[1;32m'
 GREEN='\033[0;32m'
 BOLD_YELLOW='\033[1;33m'
 YELLOW='\033[0;33m'
+BOLD_BLUE='\033[1;34m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
@@ -31,14 +34,14 @@ NC='\033[0m'
 # Display ASCII Banner #
 # ######################
 show_ascii_banner() {
-    echo -e "${BLUE}"
+    echo -e "${BOLD_BLUE}"
     echo "              _____    ___  _                "
     echo " _ __   ___   \_   \  / _ \| |_   ___   _ __ "
     echo "| '__| / _ \   / /\/ / /_)/| __| / _ \ | '__|"
     echo "| |   |  __//\/ /_  / ___/ | |_ | (_) || |   "
     echo "|_|    \___|\____/  \/      \__| \___/ |_|   "
-    echo -e "${BOLD_YELLOW}"
-    echo "                                        v1.0 "
+    echo -e "${BLUE}                   v1.0       "
+    echo -e "${BOLD_RED}     Where you were isn't where you are.     "
     echo -e "${NC}\n"
 }
 
@@ -73,56 +76,104 @@ detect_linux_distro() {
 # Install required packages #
 #############################
 install_packages() {
-    echo -e "${BLUE}[i]:${NC} Installing required packages [curl, tor, jq, xxd]..."
+    echo -e "${BLUE}[i]${NC} Checking availability of required packages..."
+
+    # Array to store the required packages
+    REQUIRED_PACKAGES=(curl tor jq xxd)
+    # Array to store the missing packages
+    MISSING_PACKAGES=()
+
+    # Look for the missing packages
+    for pkg in "${REQUIRED_PACKAGES[@]}"; do
+        if ! command -v "${pkg}" >/dev/null 2>&1; then
+            MISSING_PACKAGES+=("${pkg}")
+        fi
+    done
+
+    # Move on if all required packages are installed already
+    if [ ${#MISSING_PACKAGES[@]} -eq 0 ]; then
+        echo -e "${GREEN}[*]${NC} All required packages are already installed."
+        return
+    fi
+
+    # Install the missing packages based on the Linux Distro
+    echo -e "${BLUE}[i]${NC} Installing required packages..."
+    local INSTALL_ATTEMPTED=false
 
     case "${LINUX_DISTRO}" in
     arch | manjaro | blackarch)
-        pacman -S --needed --noconfirm curl tor jq xxd >/dev/null 2>&1
-        TOR_GROUP="tor"
+        INSTALL_ATTEMPTED=true
+        pacman -S --needed --noconfirm "${MISSING_PACKAGES[@]}" >/dev/null 2>&1
         ;;
     debian | ubuntu | kali | parrot)
+        INSTALL_ATTEMPTED=true
         apt update >/dev/null 2>&1
-        apt install -y curl tor jq xxd >/dev/null 2>&1
-        TOR_GROUP="debian-tor"
+        apt install -y "${MISSING_PACKAGES[@]}" >/dev/null 2>&1
         ;;
     fedora)
-        dnf install -y curl tor jq xxd >/dev/null 2>&1
-        TOR_GROUP="tor"
+        INSTALL_ATTEMPTED=true
+        dnf install -y "${MISSING_PACKAGES[@]}" >/dev/null 2>&1
         ;;
     opensuse)
-        zypper install -y curl tor jq xxd >/dev/null 2>&1
-        TOR_GROUP="tor"
+        INSTALL_ATTEMPTED=true
+        zypper install -y ${MISSING_PACKAGES[@]} >/dev/null 2>&1
         ;;
     *)
-        echo -e "${RED}[ERROR]:${NC} Unsupported Linux distribution. Install 'curl, tor, jq, xxd' manually on your system.\n"
-        exit 1
+        echo -e "${RED}[ERROR]:${NC} Unsupported Linux distribution. Install '${MISSING_PACKAGES[@]}' manually on your system.\n"}
         ;;
     esac
 
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}[*]${NC} Packages installed successfully."
-    else
-        echo -e "$RED[ERROR]:${NC} Package installation failed unexpectadely."
+    # Exit if Install Attempt is made and got unsuccessful in it
+    if ${INSTALL_ATTEMPTED} && [ $? -ne 0 ]; then
+        echo -e "${RED}[ERROR]${NC} Package installation failed unexpectadely."
         exit 1
+    elif ${INSTALL_ATTEMPTED}; then
+        echo -e "${GREEN}[*]:${NC} All required packages installed successfully."
     fi
 }
 
-#################################
-# Add Current User to TOR group #
-#################################
+#################################################
+# Create a TOR Group and add Current User to it #
+#################################################
 configure_user_group() {
+    # Declare a TOR Group based on Linux distribution
+    case "${LINUX_DISTRO}" in
+    arch | blackarch | manjaro | fedora | opensuse)
+        TOR_GROUP="tor"
+        ;;
+    debian | ubuntu | kali | parrot)
+        TOR_GROUP="debian-tor"
+        ;;
+    *)
+        echo -e "${RED}[x]${NC} Unable to determine TOR group for your Linux Distribution."
+        echo -e "${YELLOW}[w]${NC} Please create a TOR Group and add your current user into it manually for your system."
+        return
+        ;;
+    esac
+
     # Create a TOR Service group if not already existing
     if ! getent group "${TOR_GROUP}" >/dev/null; then
         echo -e "${YELLOW}[w]${NC} Group '${TOR_GROUP}' not found. Creating it..."
-        groupadd "${TOR_GROUP}"
-        echo -e "${GREEN}[*]${NC} Group '${TOR_GROUP}' created successfully."
+
+        if groupadd "${TOR_GROUP}" >/dev/null 2>&1; then
+            echo -e "${GREEN}[*]${NC} Group '${TOR_GROUP}' created successfully."
+        else
+            echo -e "${RED}[ERROR]${NC} Failed to create group '${TOR_GROUP}'"
+            return
+        fi
+    else
+        echo -e "${GREEN}[*]${NC} Group '${TOR_GROUP}' already exist."
     fi
 
     # Add the current user to the TOR Group
     if ! groups "$SUDO_USER" | grep -q "\b${TOR_GROUP}\b"; then
         echo -e "${BLUE}[i]${NC} Adding user '$SUDO_USER' to group '${TOR_GROUP}' ..."
-        usermod -aG "${TOR_GROUP}" "$SUDO_USER"
-        echo -e "${GREEN}[*]${NC} User '$SUDO_USER' added to group '${TOR_GROUP}' successfully."
+
+        if usermod -aG "${TOR_GROUP}" "$SUDO_USER" >/dev/null 2>&1; then
+            echo -e "${GREEN}[*]${NC} User '$SUDO_USER' added to group '${TOR_GROUP}' successfully."
+        else
+            echo -e "${RED}[ERROR]${NC} Failed to '${SUDO_USER}' to group '${TOR_GROUP}'"
+        fi
     else
         echo -e "${GREEN}[*]${NC} User '$SUDO_USER' is already part of group '${TOR_GROUP}'"
     fi
@@ -143,7 +194,7 @@ configure_tor() {
 
     # Set up and update the TORRC with required parameters
     if [ "${NEEDS_UPDATE}" -eq 1 ]; then
-        echo -e "${BLUE}[i]${NC} Updating TORRC with required ControlPort settings..."
+        echo -e "${BLUE}[i]${NC} Updating TORRC with required TOR Network settings..."
         {
             echo ""
             echo "# Configurations added by [reIPtor]"
@@ -155,7 +206,7 @@ configure_tor() {
         systemctl restart tor >/dev/null 2>&1
         echo -e "${GREEN}[*]${NC} TOR Service configured successfully."
     else
-        echo -e "${GREEN}[i]${NC} TOR Service is already configured, Skipping updates..."
+        echo -e "${GREEN}[i]${NC} TOR Service is already configured, Skipping configs..."
     fi
 }
 
@@ -183,7 +234,8 @@ main() {
     configure_tor
 
     # Get the interval from the User to change the TOR IP
-    read -p "Enter time interval to change TOR IP (in sec) [default is 15s]: " TIME_INTERVAL
+    echo ""
+    read -p "Enter time interval to change your IP (in sec) [default is 15s]: " TIME_INTERVAL
     TIME_INTERVAL=${TIME_INTERVAL:-15}
 
     echo -e "${BLUE}[i]${NC} Changing your TOR IP at every ${TIME_INTERVAL} seconds..."
